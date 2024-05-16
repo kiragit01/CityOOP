7b758c9a3f107834141349eab9bdaf0ba7bca1fb
kiragit01 <kiragithub@gmail.com>
2024-05-16 21:37:55 +0300

    Добавьте файлы проекта.

---
+++ Building.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityOOP
+{
+    class Building
+    {
+        public string Title {  get; set; }
+        public int Persons { get; set; }
+
+        public Building() : this("Unidentified", 0) { }
+        public Building(string title) : this(title, 0) { }
+        public Building(int persons) : this("Unidentified", persons) { }
+        public Building(string title, int persons) 
+        {
+            Title = title;
+            Persons = persons;
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"\nВ здании \"{Title}\" находиться {Persons} человек.");
+        }
+    }
+
+    class Hospital : Building
+    {
+        public int Patients { get; set; }
+        public Hospital() { }
+        public Hospital(string title, int persons, int patients) 
+            : base(title, persons)
+        {
+            Patients = patients;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"На данный момент в \"{Title}\" лечатся {Patients} пациентов.");
+        }
+    }
+
+    class Residence : Building
+    {
+        public string President { get; set; }
+        public Residence() { }
+        public Residence(int persons, string president) 
+            : base("Резиденция", persons)
+        {
+            President = president;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Недавно был назначен новый президент: {President}.");
+        }
+    }
+
+    class Police : Building
+    {
+        public int Criminals { get; set; }
+        public Police() { }
+        public Police(string title, int persons, int criminals)
+            : base(title, persons)
+        {
+            Criminals = criminals;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"В этом полицейском участке находиться {Criminals} очень опасных преступников.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityOOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Building hospital = new Hospital
+                (
+                title: "Скорая помощь", 
+                persons: 500, 
+                patients: 350
+                );
+            hospital.Print();
+
+            Building residence = new Residence
+                (
+                persons: 200, 
+                president: "Вася Пупкин"
+                );
+            residence.Print();
+
+            Building police = new Police
+                (
+                title: "Защита мира",
+                persons: 120,
+                criminals: 30
+                );
+            police.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
